fd4bbbffdee29738d3e686ce5bbd7f60716c14a4
mbp127 <mp127@hdm-stuttgart.de>
2018-03-14 19:57:21 +0100

    findAll

--- WorkTime/src/de/hdm/ITProjekt/WorkTime/server/datenbank/UserMapper.java
+++ WorkTime/src/de/hdm/ITProjekt/WorkTime/server/datenbank/UserMapper.java
@@ -2,25 +2,37 @@ package de.hdm.ITProjekt.WorkTime.server.datenbank;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 
 import de.hdm.ITProjekt.WorkTime.shared.User;
 
 public class UserMapper {
-	
-	public static void findAll() {
-		DBConnection con = Connection.connection();
-		Vector <User> result = new Vector <User>();
-		
+
+	public static Vector<User> findAll() {
+		Connection con = DBConnection.connection();
+		Vector<User> result = new Vector<User>();
+
 		try {
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("SELECT * FROM User ORDER BY name asc");
-			
+
 			while (rs.next()) {
-				u.setId(rs.getInt(id));
+				User u = new User();
+				u.setId(rs.getInt("id"));
+				u.setEmail(rs.getString("email"));
+				u.setName(rs.getString("name"));
+				u.setPasswort(rs.getString("passwort"));
+				u.setTyp(rs.getInt("typ"));
+				u.setVorname(rs.getString("vorname"));
+
 			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		
+		return result;
 	}
 
 }
