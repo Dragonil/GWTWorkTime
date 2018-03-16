d678b93afe6f0e0a41a3f15bd0aad73a5aeb4732
Oli Gorges <oli@141.62.89.211>
2018-03-16 17:34:23 +0100

    WorkTime

--- WorkTime/src/de/hdm/ITProjekt/WorkTime/client/WorkTime.java
+++ WorkTime/src/de/hdm/ITProjekt/WorkTime/client/WorkTime.java
@@ -1,44 +1,94 @@
-
 package de.hdm.ITProjekt.WorkTime.client;
 
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.datepicker.client.DateBox;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 
-
-public class WorkTime implements EntryPoint { // Kanban
+public class WorkTime implements EntryPoint {
+	
+	protected static int currentModule = 3;
+	private static FlowPanel content = new FlowPanel(); // ContentPanel
 	
 	
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+	// Aufgaben
+	private static DateBox date = new DateBox();
+	private static TextBox titel = new TextBox();
+	private static TextArea beschreibung = new TextArea();
+	private static ListBox prio = new ListBox();
+	
+	// DialogBox
+	private static DialogBox dialog;
+	private static Button close = new Button("OK");
+	private static VerticalPanel dialogPanel;
 
-
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad()  {
-			/*
-		    DockLayoutPanel p = new DockLayoutPanel(Unit.EM);
-		    p.addNorth(new HTML("north"), 2);
-		    p.addSouth(new HTML("south"), 2);
-		    p.addEast(new HTML("east"), 2);
-		    p.addWest(new HTML("west"), 2);
-		    p.add(new HTML("center"));
-
-		    // Attach the LayoutPanel to the RootLayoutPanel. The latter will listen for
-		    // resize events on the window to ensure that its children are informed of
-		    // possible size changes.
-		    RootLayoutPanel rp = RootLayoutPanel.get();
-		    rp.add(p);
-		    */
-		
-			RootPanel.get().add(Kanban.getPanel());
-		  }
-		
+	public void onModuleLoad() {
+		update();
 	}
+	
+	public static void update() {
+		
+		RootPanel.get("content").clear();
+		Panel p = null;
+		switch(currentModule){
+		case 1: p = Login.getPanel(); break;
+		case 2: p = Dashboard(); break;
+		case 3: p = Kanban.getPanel(); break;
+		}
+		
 
+		RootPanel.get("content").add(p);
+		//RootPanel.get("content").add(dialog);
+	}
+	/*
+	private Panel Login(){
+		VerticalPanel vp = new VerticalPanel();
+		
+		
+		login.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent e){
+				currentModule++;
+				onModuleLoad();
+			}
+		});
+		
+		vp.add(new HTML("<Label>EMail:</Label>"));
+		vp.add(email);
+		vp.add(new HTML("<Label>Passwort:</Label>"));
+		vp.add(password);
+		vp.add(login);
+		return vp;
+	}
+	*/
+	
+	private static Panel Dashboard(){
+		VerticalPanel vp = new VerticalPanel();
+		HorizontalPanel hp = new HorizontalPanel();
+		FocusPanel nav[] = {new FocusPanel(new HTML("NaviBox1")), new FocusPanel(new HTML("NaviBox2")), new FocusPanel(new HTML("NaviBox3"))};
+
+		hp.addStyleName("naviDiv");
+		for(FocusPanel p : nav) {
+			
+			p.addStyleName("naviElement");
+			p.addClickHandler(new ClickHandler(){
+				public void onClick(ClickEvent e){
+					Window.alert(e.toString());
+				}
+			});
+			
+			hp.add(p);
+		}
+		
+		vp.add(hp);
+		vp.add(new HTML("<h1>Test</h1>"));
+		vp.add(content);
+		return vp;
+	}
+}
