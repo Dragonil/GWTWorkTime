3ce488edf61cf9a70190b0df0e41fbecb4afcd1c
Janina Mattes <jm105@hdm-stuttgart.de>
2018-03-18 20:01:29 +0100

    "First GUI Update"

--- WorkTime/src/de/hdm/ITProjekt/WorkTime/client/WorkTime.java
+++ WorkTime/src/de/hdm/ITProjekt/WorkTime/client/WorkTime.java
@@ -1,10 +1,25 @@
 package de.hdm.ITProjekt.WorkTime.client;
 
+import java.util.Vector;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.hdm.ITProjekt.WorkTime.shared.AufgabenAdministration;
 import de.hdm.ITProjekt.WorkTime.shared.AufgabenAdministrationAsync;
+import de.hdm.ITProjekt.WorkTime.shared.bo.Aufgabe;
 
 
 public class WorkTime implements EntryPoint {
@@ -23,11 +38,11 @@ public class WorkTime implements EntryPoint {
 	private TextArea inhalt = new TextArea();
 	
 	private Label titleLabel = new Label("Titel:");
-	private Label untertitelLabel = new Label("Untertitel:");
+	private Label beschreibung = new Label("Bescchreibung:");
 	private Label inhaltLabel = new Label("Text:");
 	private Label errorLabel = new Label();
 	
-	private Label html = new HTML("<h1>Neuer Blogeintrag Nr. " + counter + " anlegen </h1>");
+	private Label html = new HTML("<h1> You have" + counter + " tasks to be done. </h1>");
 	
 	private Button saveBtn = new Button("Speichern");
 	
@@ -47,7 +62,7 @@ public class WorkTime implements EntryPoint {
 			fields.setWidget(0, 0, new HTML("<b>Eintrag</b>"));
 			fields.setWidget(1, 0, titleLabel);
 			fields.setWidget(1, 1, title);
-			fields.setWidget(2, 0, untertitelLabel);
+			fields.setWidget(2, 0, beschreibung);
 			fields.setWidget(2, 1, untertitel);
 			fields.setWidget(3, 0, inhaltLabel);
 			fields.setWidget(3, 1, inhalt);
@@ -71,7 +86,7 @@ public class WorkTime implements EntryPoint {
 			
 			public void addBlogeintrag(){
 				
-				taskControll.createBlogeintrag(inhalt.getText(), title.getText(), untertitel.getText(), new AsyncCallback<Blogeintrag>() {
+				taskControll.createAufgabe(title.getText(), beschreibung.getText(), new AsyncCallback <Aufgabe>() {
 
 					@Override
 					public void onFailure(Throwable caught) {
@@ -79,11 +94,11 @@ public class WorkTime implements EntryPoint {
 						
 					}
 
+
 					@Override
-					public void onSuccess(Blogeintrag result) {
-						if( result != null) {
-							updateCounter();				
-						}
+					public void onSuccess(Aufgabe result) {
+						// TODO Auto-generated method stub
+						
 					}
 					
 				});
@@ -93,13 +108,13 @@ public class WorkTime implements EntryPoint {
 
 		public void updateCounter() {
 			
-			taskControll.findAll(new AsyncCallback<Vector<Blogeintrag>>(){
+			taskControll.findAllTasks(new AsyncCallback <Vector<Aufgabe>>(){
 				
 				public void onFailure(Throwable caught) {
 					Window.alert(SERVER_ERROR);
 				}
 				
-				public void onSuccess(Vector<Blogeintrag> be) {
+				public void onSuccess(Vector<Aufgabe> be) {
 					if(be!=null) {
 						counter = be.size() + 1;
 					}
