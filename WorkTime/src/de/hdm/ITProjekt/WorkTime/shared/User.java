88d6d5632ce83a056cc45ea06ac6087da2ff4374
MarcoPracher <mp127@hdm-stuttgart.de>
2018-04-10 15:46:40 +0200

    xxxx

--- WorkTime/src/de/hdm/ITProjekt/WorkTime/shared/User.java
+++ WorkTime/src/de/hdm/ITProjekt/WorkTime/shared/User.java
@@ -14,7 +14,7 @@ public class User implements Serializable {
 	private String passwort;
 	private Date letzterLogin;
 	private int typ;
-	private int plz2;
+	private int plz222;
 
 	public User() {
 
